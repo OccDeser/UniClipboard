6c18f30a0928f629ba90d5380778997d3da59ecb
OccDeser <2287109950@qq.com>
2022-09-08 20:02:25 +0800

    fix bugs in color print on windows

--- src/common/message.rs
+++ src/common/message.rs
@@ -1,7 +1,4 @@
-extern crate colorful;
-
-use colorful::Colorful;
-use colorful::HSL;
+use term_painter::{Color, ToStyle};
 
 pub fn welcome() {
     let s0 = "             _   ___ _ _       _                         _";
@@ -10,24 +7,24 @@ pub fn welcome() {
     let s3 = "\\ \\_/ / | | | / /___| | | |_) | |_) | (_) | (_| | | | (_| |";
     let s4 = " \\___/|_| |_|_\\____/|_|_| .__/|_.__/ \\___/ \\__,_|_|  \\__,_|";
     let s5 = "                        |_|                                    ";
-    println!("{}", s0.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));
-    println!("{}", s1.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));
-    println!("{}", s2.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));
-    println!("{}", s3.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));
-    println!("{}", s4.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));
-    println!("{}", s5.gradient_with_color(HSL::new(0.0, 1.0, 0.5), HSL::new(0.833, 1.0, 0.5)));}
+    println!("{}", s0);
+    println!("{}", s1);
+    println!("{}", s2);
+    println!("{}", s3);
+    println!("{}", s4);
+    println!("{}", s5);
+}
 
 pub fn success(key: String, msg: String) {
-    // let key = format!("{: <7}", key);
-    println!("{}: {}", key.green().bold(), msg);
+    println!("{}: {}", Color::Green.bold().paint(key), msg);    
 }
 
 pub fn warning(msg: String) {
-    println!("{}: {}", "warning".yellow().bold(), msg);
+    println!("{}: {}", Color::Yellow.bold().paint("warning"), msg);
 }
 
 pub fn error(msg: String) {
-    println!("{}: {}", "error".red().bold(), msg);
+    println!("{}: {}", Color::Red.bold().paint("error"), msg);
 }
 
 pub fn info(msg: String) {
